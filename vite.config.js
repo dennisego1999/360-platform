631d735b138a2cdeba141dab3a65a189c94ad15a
Dennis Ego <dennis.ego@artcoresociety.com>
2024-01-11 11:17:22 +0100

    add eslint

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,27 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-});
+const config = {
+	plugins: [
+		laravel({
+			input: 'resources/js/app.js',
+			ssr: 'resources/js/ssr.js',
+			refresh: true
+		}),
+		vue({
+			template: {
+				transformAssetUrls: {
+					base: null,
+					includeAbsolute: false
+				}
+			}
+		})
+	],
+	resolve: {
+		alias: {
+			'@': '/resources/js'
+		}
+	}
+};
+
+export default defineConfig(config);
