bf7db635f13356330bed676681755abefe36dafd
Dennis Ego <dennis.ego@artcoresociety.com>
2024-01-11 12:22:05 +0100

    add toastifications and vue-i18n

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 use Inertia\Middleware;
-use Tightenco\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,19 +5,83 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { router } from '@inertiajs/vue3';
+import { createI18n } from 'vue-i18n';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// Setup toast options
+const options = {
+	transition: 'Vue-Toastification__fade',
+	maxToasts: 5,
+	position: 'top-center',
+	timeout: 5000,
+	container: document.querySelector('#app'),
+	pauseOnFocusLoss: true,
+	pauseOnHover: true,
+	closeOnClick: true,
+	closeButton: 'button',
+	icon: false,
+	newestOnTop: true,
+	draggable: true,
+	draggablePercent: 0.6,
+	showCloseButtonOnHover: false
+};
 
+// Create a new Vue app instance for Inertia
 createInertiaApp({
-	title: (title) => `${title} - ${appName}`,
+	title: (title) => {
+		const appName = import.meta.env.VITE_APP_NAME;
+
+		if (!title) {
+			return appName;
+		}
+
+		return `${title} - ${appName}`;
+	},
 	resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
 	setup({ el, App, props, plugin }) {
-		return createApp({ render: () => h(App, props) })
+		const i18n = createI18n({
+			locale: props.initialPage.props.locales.currentLocale,
+			messages: props.initialPage.props.translations,
+			legacy: false,
+			warnHtmlMessage: false
+		});
+
+		return createApp({
+			created() {
+				router.on('invalid', (event) => {
+					// Do not prevent the default behavior while developing
+					if (process.env.NODE_ENV !== 'production') {
+						return;
+					}
+
+					// Do not prevent the handler for server errors or validation checks
+					if (event.detail.response.status === 500 || event.detail.response.status === 403) {
+						return;
+					}
+
+					// Prevent a white modal
+					event.preventDefault();
+
+					// Log the invalid response info
+					console.error('An invalid Inertia response was received.');
+					console.error(event.detail.response);
+				});
+			},
+
+			mounted() {
+				// Remove the props to initialize Vue
+				document.getElementById('app').removeAttribute('data-page');
+			},
+
+			render: () => h(App, props)
+		})
 			.use(plugin)
 			.use(ZiggyVue)
+			.use(Toast, options)
+			.use(i18n)
 			.mount(el);
 	},
-	progress: {
-		color: '#4B5563'
-	}
+	progress: { color: 'var(--color-primary)' }
 });
