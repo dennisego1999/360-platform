813ea8f3fd59fbcb867de9d3eccf17d088080933
Dennis Ego <dennis.ego@artcoresociety.com>
2024-01-11 12:24:56 +0100

    listen for flash success or error messages in applayout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,16 +1,20 @@
 <script setup>
-import { ref } from 'vue';
-import { Head, Link, router } from '@inertiajs/vue3';
+import { computed, ref, watch } from 'vue';
+import { Head, Link, router, usePage } from '@inertiajs/vue3';
 import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Banner from '@/Components/Banner.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { useClearToast, useShowToast } from '@/Composables/Toastification.js';
 
 defineProps({
 	title: String
 });
+
+// Get the UUID
+const flashUuid = computed(() => usePage().props.flash.uuid);
 
 const showingNavigationDropdown = ref(false);
 
@@ -29,6 +33,64 @@ const switchToTeam = (team) => {
 const logout = () => {
 	router.post(route('logout'));
 };
+
+// Handle success messages
+watch(
+	() => usePage().props.flash.success,
+	(message) => {
+		if (!message || !flashUuid.value) {
+			return;
+		}
+
+		// Abort if we already had this flashed message
+		if (sessionStorage.getItem('flashSuccess') === flashUuid.value) {
+			return;
+		}
+
+		// Toast the flashed success message
+		const successId = 'flash-success-toast';
+
+		useClearToast(successId);
+		useShowToast(null, message.toString(), 'success', {
+			id: successId
+		});
+
+		usePage().props.flash.success = null;
+		sessionStorage.setItem('flashSuccess', flashUuid.value);
+	},
+	{
+		immediate: true
+	}
+);
+
+// Handle error messages
+watch(
+	() => usePage().props.flash.error,
+	(message) => {
+		if (!message || !flashUuid.value) {
+			return;
+		}
+
+		// Abort if we already had this flashed message
+		if (sessionStorage.getItem('flashError') === flashUuid.value) {
+			return;
+		}
+
+		// Toast the flashed error
+		const error = 'flash-error-toast';
+
+		useClearToast(error);
+		useShowToast(null, message.toString(), 'error', {
+			id: error
+		});
+
+		usePage().props.flash.error = null;
+		sessionStorage.setItem('flashError', flashUuid.value);
+	},
+	{
+		immediate: true
+	}
+);
 </script>
 
 <template>
