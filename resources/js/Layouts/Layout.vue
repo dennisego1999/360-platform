e28387a7a7a1d47896915e33ef1ee9256cd74232
Dennis Ego <dennis.ego@artcoresociety.com>
2024-01-11 13:12:44 +0100

    add layout structure

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { computed, watch } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+import { useClearToast, useShowToast } from '@/Composables/Toastification.js';
+
+// Get the UUID
+const flashUuid = computed(() => usePage().props.flash.uuid);
+
+// Handle success messages
+watch(
+	() => usePage().props.flash.success,
+	(message) => {
+		if (!message || !flashUuid.value) {
+			return;
+		}
+
+		// Abort if we already had this flashed message
+		if (sessionStorage.getItem('flashSuccess') === flashUuid.value) {
+			return;
+		}
+
+		// Toast the flashed success message
+		const successId = 'flash-success-toast';
+
+		useClearToast(successId);
+		useShowToast(null, message.toString(), 'success', {
+			id: successId
+		});
+
+		usePage().props.flash.success = null;
+		sessionStorage.setItem('flashSuccess', flashUuid.value);
+	},
+	{
+		immediate: true
+	}
+);
+
+// Handle error messages
+watch(
+	() => usePage().props.flash.error,
+	(message) => {
+		if (!message || !flashUuid.value) {
+			return;
+		}
+
+		// Abort if we already had this flashed message
+		if (sessionStorage.getItem('flashError') === flashUuid.value) {
+			return;
+		}
+
+		// Toast the flashed error
+		const error = 'flash-error-toast';
+
+		useClearToast(error);
+		useShowToast(null, message.toString(), 'error', {
+			id: error
+		});
+
+		usePage().props.flash.error = null;
+		sessionStorage.setItem('flashError', flashUuid.value);
+	},
+	{
+		immediate: true
+	}
+);
+</script>
+
+<template>
+	<slot />
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Layout from '@/Layouts/Layout.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+// Define options
+defineOptions({
+	layout: [Layout, AppLayout]
+});
+</script>
+
+<template>
+	<p>Home</p>
+</template>
